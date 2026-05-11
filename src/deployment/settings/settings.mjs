578cc62a57dbb87d8197acdb06be8c50c76d3456
ali46000-source <ali46000@auburnschl.edu>
2026-05-11 10:02:20 -0400

    mod 13

--- src/deployment/settings/settings.mjs
+++ src/deployment/settings/settings.mjs
@@ -1,23 +1,9 @@
 /** @type {import('../types/settings').DeploymentSettings<'render.com'>} */
 const deploymentSettings = {
-<<<<<<< HEAD
-  host: 'render.com',
-  type: 'static',
-=======
   host: 'netlify',
   type: 'static',
->>>>>>> 4adeada54c0affd2fc1451884ce1a64855fa5a09
   services: [
     {
-<<<<<<< HEAD
-      name: 'api',
-      type: 'static',
-      routePrefix: '/api',
-      localPort: 8787
-    },
-    {
-=======
->>>>>>> 4adeada54c0affd2fc1451884ce1a64855fa5a09
       name: 'multiplayer',
       type: 'go',
       routePrefix: '/api/multiplayer',
